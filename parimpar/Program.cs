37e810ae678cc3135dfa49f9f8e6a3e2a7255748
jumis <jumis@DESKTOP-I6AFV1V>
2024-04-18 15:42:17 -0300

    Add project files.

---
+++ parimpar/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Informe um valor inteiro");
+int num = Convert.ToInt32 (Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Você informou um número par");
+}
+else
+{
+    Console.WriteLine("Você informou um número ímpar");
+}
